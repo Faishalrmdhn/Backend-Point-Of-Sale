d19971c34be2a2007de053b8ae89b2c5b5dab6d3
Faishalrmdhn <faishalramadhan0@gmail.com>
2020-09-07 19:35:51 +0700

    fix config env

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,9 +1,10 @@
+require("dotenv/config");
 const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "onlineshop",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((error) => {
