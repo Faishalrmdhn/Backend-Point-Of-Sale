ecfa8bc82df67ccb0b74be56dea20b575d23fc80
Faishalrmdhn <faishalramadhan0@gmail.com>
2020-10-07 18:25:57 +0700

    fixing bug

--- index.js
+++ index.js
@@ -7,49 +7,6 @@ const cors = require("cors");
 const routerNavigation = require("./src");
 
 const app = express();
-// =======================================================
-// const socket = require("socket.io");
-// =======================================================
-// =======================================================
-// const http = require("http");
-// const server = http.createServer(app);
-// const io = socket(server);
-
-// io.on("connection", (socket) => {
-//   console.log("Socket.io connected...");
-//   socket.on("globalMessage", (data) => {
-//     io.emit("chatMessage", data);
-//   });
-
-//   socket.on("privateMessage", (data) => {
-//     socket.emit("chatMessage", data);
-//   });
-
-//   socket.on("broadcastMessage", (data) => {
-//     console.log(data);
-//     socket.broadcast.emit("chatMessage", data);
-//   });
-
-//   socket.on("welcomeMessage", (data) => {
-//     socket.emit("chatMessage", {
-//       user_name: "BOTMEE6",
-//       message: `Welcome back ${data.user_name}!`,
-//     });
-
-//     socket.broadcast.emit("chatMessage", {
-//       user_name: "BOTMEE6",
-//       message: `${data.user_name} has joined the chat`,
-//     });
-//   });
-
-//   // spesifik
-//   socket.on("'typing", (data) => {
-//     socket.broadcast.emit("typingMessage", data);
-//   });
-
-//   socket.join(data.room);
-// });
-// =======================================================
 
 app.use(cors());
 app.use(bodyParser.json()); //mengambi data pada raw/json
