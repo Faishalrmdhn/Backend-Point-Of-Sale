c81c4fc5f6f22b5a699f57d80ab328ccebc7a7f9
faishalrmdhn <faishalramadhan25@gmail.com>
2020-11-26 17:10:40 +0700

    clean code index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 require("dotenv").config();
 
-const bodyParser = require("body-parser"); //middleware/menangkap data pada postman
-const morgan = require("morgan"); //melihat aktivitas user (post/get/delete/patch)
+const bodyParser = require("body-parser"); 
+const morgan = require("morgan"); 
 const cors = require("cors");
 const routerNavigation = require("./src");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json()); //mengambi data pada raw/json
-app.use(bodyParser.urlencoded({ extended: false })); //mengambil data pada body url
-app.use(morgan("dev")); //dev = format
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(morgan("dev")); 
 app.use(express.static("uploads"));
 app.use((request, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
@@ -32,9 +32,3 @@ app.listen(process.env.PORT, process.env.IP, () => {
     `Express app is listening on Host: ${process.env.IP} and port: ${process.env.PORT}`
   );
 });
-// =======================================================
-
-// server.listen(3000, () => {
-//   console.log("listening on port 3000");
-// });
-// =======================================================
